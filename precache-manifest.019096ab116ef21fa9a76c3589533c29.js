210686720a5d488d3a22743e4d0c208641006944
Muhammedyaseen007 <84324698+Muhammedyaseen007@users.noreply.github.com>
2021-05-22 19:59:43 +0530

    Add files via upload

---
+++ precache-manifest.019096ab116ef21fa9a76c3589533c29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d70d4739d5d287c4c9",
+    "url": "/static/css/main.1cf952cb.chunk.css"
+  },
+  {
+    "revision": "f0d70d4739d5d287c4c9",
+    "url": "/static/js/main.f0d70d47.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "821855c891b43d4a21b4266eb1d9415f",
+    "url": "/index.html"
+  }
+];
